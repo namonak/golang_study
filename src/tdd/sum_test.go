eb7aa18f6dbda8e11068e1f3ca3a764a93b02182
namonak <namonak@gmail.com>
2019-08-04 02:50:58 +0900

    Added test code for random number of variable arguments.

--- src/tdd/sum_test.go
+++ src/tdd/sum_test.go
@@ -9,7 +9,11 @@ func Test_SumInt32(t *testing.T) {
 	loopCount := 1000
 	
 	for i := 0; i <= loopCount; i++ {
-		a := []int32{rand.Int31n(10000), rand.Int31n(10000), rand.Int31n(10000), rand.Int31n(10000)}
+		varArgCnt := rand.Intn(10)
+		var a []int32
+		for j := 0; j < varArgCnt; j++ {
+			a = make([]int32, j+1)
+		}
 		actual := SumInt32(a...)
 		var expected int32
 		for _, value := range a {
